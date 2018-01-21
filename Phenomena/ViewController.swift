724054c15156ff26e84bdd7fa4a402667fcc80c2
toedwy <toedwy79@gmail.com>
2018-01-21 17:25:03 +0800

    try to contribute

--- Phenomena/ViewController.swift
+++ Phenomena/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
