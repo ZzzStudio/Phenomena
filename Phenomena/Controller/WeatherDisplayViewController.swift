0485fe1d8ca678ca0edd866398c7a36080f781f0
toedwy <toedwy79@gmail.com>
2018-01-25 01:05:33 +0800

    add main display vc

---
+++ Phenomena/Controller/WeatherDisplayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherDisplayViewController.swift
+//  Phenomena
+//
+//  Created by toedwy on 2018/1/25.
+//  Copyright © 2018年 Zzz Studio. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
