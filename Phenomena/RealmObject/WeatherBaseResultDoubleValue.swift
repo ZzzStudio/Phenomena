fbb8329c5aafa3a8afc7ca36edc652e725f2e24f
toedwy <toedwy79@gmail.com>
2018-02-06 23:57:16 +0800

    add some weather forecast RLM O

---
+++ Phenomena/RealmObject/WeatherBaseResultDoubleValue.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherBaseResultDoubleValue.swift
+//  Phenomena
+//
+//  Created by toedwy on 2018/2/6.
+//Copyright © 2018年 Zzz Studio. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class WeatherBaseResultDoubleValue: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
