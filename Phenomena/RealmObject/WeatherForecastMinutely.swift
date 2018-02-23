67b72e78b198547ddf2446a9a972f7d66486bcf5
toedwy <toedwy79@gmail.com>
2018-02-23 23:36:11 +0800

    minutely RLM O

--- Phenomena/RealmObject/WeatherForecastMinutely.swift
+++ Phenomena/RealmObject/WeatherForecastMinutely.swift
@@ -10,20 +10,47 @@ import Foundation
 import RealmSwift
 import ObjectMapper
 
-//class WeatherForecastMinutely: Object, Mappable {
-//    
-//    @objc dynamic var <#property#>
-//    
-////    override static func primaryKey() -> String? {
-////        return ""
-////    }
-//    
-////    required convenience init?(map: Map) {
-////        self.init()
-////    }
-//    
-////    func mapping(map: Map) {
-////        <#property#> <- map[<#key#>]
-////    }
-//}
+class WeatherForecastMinutely: Object, Mappable {
+    
+    @objc dynamic var status: String?
+    @objc dynamic var descriptionMinutely: String?
+    var probability = List<DoubleObject>()
+    var precipitation2h = List<DoubleObject>()
+    var precipitation = List<DoubleObject>()
+    
+    //    override static func primaryKey() -> String? {
+    //        return ""
+    //    }
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        status                  <- map["status"]
+        descriptionMinutely     <- map["description"]
+        
+        var probability:[Double]? = nil
+        probability <- map["probability"]
+        probability?.forEach { value in
+            let c = DoubleObject()
+            c.value = value
+            self.probability.append(c)
+        }
+        var precipitation2h:[Double]? = nil
+        precipitation2h <- map["precipitation_2h"]
+        precipitation2h?.forEach { value in
+            let c = DoubleObject()
+            c.value = value
+            self.precipitation2h.append(c)
+        }
+        var precipitation:[Double]? = nil
+        precipitation <- map["precipitation"]
+        precipitation?.forEach { value in
+            let c = DoubleObject()
+            c.value = value
+            self.precipitation.append(c)
+        }
+    }
+}
 
