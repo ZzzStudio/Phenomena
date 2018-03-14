ee4c17610d073430744ebd8a9ebc396288fe3372
toedwy <toedwy79@gmail.com>
2018-03-15 00:39:30 +0800

    add latest weather collection view cell

---
+++ Phenomena/View/LatestWeatherCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LatestWeatherCollectionViewCell.swift
+//  Phenomena
+//
+//  Created by toedwy on 2018/3/15.
+//  Copyright © 2018年 Zzz Studio. All rights reserved.
+//
+
+import UIKit
+
+class LatestWeatherCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
