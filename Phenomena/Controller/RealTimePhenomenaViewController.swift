6d63e98feffd47be352c4f81aaeb4c4bcbe7efed
toedwy <toedwy79@gmail.com>
2018-03-17 01:13:53 +0800

    WIP: real time phenomena view

---
+++ Phenomena/Controller/RealTimePhenomenaViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RealTimePhenomenaViewController.swift
+//  Phenomena
+//
+//  Created by toedwy on 2018/3/17.
+//  Copyright © 2018年 Zzz Studio. All rights reserved.
+//
+
+import UIKit
+
+class RealTimePhenomenaViewController: UIViewController {
+    
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var pm25Label: UILabel!
+    @IBOutlet weak var cloudinessLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var precipitationLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
