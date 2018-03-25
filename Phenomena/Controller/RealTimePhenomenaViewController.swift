b2a52202fee73ce9accea5057f1aa69c0ac8071f
toedwy <toedwy79@gmail.com>
2018-03-25 21:53:02 +0800

    update forecast controller

--- Phenomena/Controller/RealTimePhenomenaViewController.swift
+++ Phenomena/Controller/RealTimePhenomenaViewController.swift
@@ -28,7 +28,6 @@ class RealTimePhenomenaViewController: UIViewController {
         super.viewDidLoad()
         
         GeographicInfomationManager.sharedManager.delegate = self;
-        
     }
 
 }
--- Phenomena/Controller/WeatherForecastViewController.swift
+++ Phenomena/Controller/WeatherForecastViewController.swift
@@ -11,14 +11,63 @@ import UIKit
 class WeatherForecastViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView .register(UINib.init(nibName: "LatestWeatherCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "LatestWeatherCollectionViewCell")
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = CGSize.init(width: 40, height: 141)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView .register(UINib.init(nibName: "WeatherForecastTableViewCell", bundle: nil), forCellReuseIdentifier: "WeatherForecastTableViewCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func setupForecast() {
+
+    }
+
+}
+
+// MARK: - UICollectionViewDelegate
+extension WeatherForecastViewController: UICollectionViewDelegate {
+
+}
+
+// MARK: - UICollectionViewDataSource
+extension WeatherForecastViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LatestWeatherCollectionViewCell", for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension WeatherForecastViewController: UITableViewDelegate {
+
+}
+
+// MARK: - UITableViewDataSource
+extension WeatherForecastViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherForecastTableViewCell", for: indexPath)
+        return cell
+    }
 }
