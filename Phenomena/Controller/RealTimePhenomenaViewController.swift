bb4d9be157a8fe17d2dd8c2315c0d3dc08453347
toedwy <toedwy79@gmail.com>
2018-04-01 00:50:31 +0800

    remove comments

--- Phenomena/Controller/RealTimePhenomenaViewController.swift
+++ Phenomena/Controller/RealTimePhenomenaViewController.swift
@@ -42,9 +42,6 @@ extension RealTimePhenomenaViewController: GeographicInfomationManagerDelegate {
             guard let realTime = result else { return }
             print("---request finished---")
 
-//            if let temperature = realTime.result?.temperature {
-//                self.temperatureLabel.text = "\(temperature)"
-//            }
             self.temperatureLabel.text = "\(realTime.result?.temperature.value ?? 0)"
             self.descriptionLabel.text = realTime.result?.skycon ?? "n/a"
 
