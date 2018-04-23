52f57f932c781e2dbb01fe4b8b6a0bf0a00c1b12
toedwy <toedwy79@gmail.com>
2018-04-23 23:37:13 +0800

    Rainy day

--- Phenomena/Controller/WeatherForecastViewController.swift
+++ Phenomena/Controller/WeatherForecastViewController.swift
@@ -42,7 +42,6 @@ class WeatherForecastViewController: UIViewController {
         WeatherDataInterface.requestWeatherForecast { (result) in
             log.debug("result")
             guard let forecast = result else { return }
-//            guard let skycon = forecast.result?.hourly?.skycon else
             
             if let hourlySkycon = forecast.result?.hourly?.skycon {
                 self.hourlySkycon = hourlySkycon
